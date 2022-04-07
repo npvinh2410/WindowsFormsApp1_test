d8453e66d843a9b073d8a5e450f67148346ac881
TranNgocVy <ngocvy04042001@gmail.com>
2022-04-07 10:20:21 +0700

    test

--- WindowsFormsApp1_test/Form1.Designer.cs
+++ WindowsFormsApp1_test/Form1.Designer.cs
@@ -29,19 +29,8 @@ namespace WindowsFormsApp1_test
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(314, 62);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkBox1
             // 
@@ -59,7 +48,6 @@ namespace WindowsFormsApp1_test
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Home";
             this.Text = " Home";
             this.ResumeLayout(false);
@@ -68,8 +56,6 @@ namespace WindowsFormsApp1_test
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
     }
 }
